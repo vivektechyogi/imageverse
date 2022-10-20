abacfea0703d4dec53d0224bc24bde5c447e16dd
Vivek <iamviveklalan@gmail.com>
2022-10-20 15:57:33 +0530

    added ImageCell file

---
+++ Imageverse/views/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCellView.swift
+//  Imageverse
+//
+//  Created by Jai Mataji on 16/10/22.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
